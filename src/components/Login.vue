47dd553d838e2a7e0b39127f9ea847fb45a4e4ca
Lin-Htet-Phyo <linhtethtetphyo181688@gmail.com>
2025-03-14 23:19:16 +0630

    signup and login component

---
+++ src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>Login</h2>
+  <form @submit.prevent="login">
+    <input type="text" placeholder="Display Name....." v-model="displayName">
+    <input type="email" placeholder="Email....." v-model="email">
+    <input type="password" placeholder="Password....." v-model="password">
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    let displayName = ref("");
+    let email = ref("");
+    let password = ref("");
+    let login = () => {
+      console.log(displayName.value);
+      console.log(email.value);
+      console.log(password.value);
+    }
+
+    return {
+      displayName,
+      email, 
+      password,
+      login,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
