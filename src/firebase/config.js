4202d1822a8946e9e3d477f90ca63bb01efda796
Lin-Htet-Phyo <linhtethtetphyo181688@gmail.com>
2025-03-14 22:58:16 +0630

    firebase config add

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCqPxTGKDYMjSMmhCfekzzI34CQ2Fl3SEk",
+    authDomain: "vue-blog-system-10857.firebaseapp.com",
+    projectId: "vue-blog-system-10857",
+    storageBucket: "vue-blog-system-10857.firebasestorage.app",
+    messagingSenderId: "843091639542",
+    appId: "1:843091639542:web:2be602d33a4a3af75c00d7"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+let timestamp = serverTimestamp();
+
+export { db, timestamp };
