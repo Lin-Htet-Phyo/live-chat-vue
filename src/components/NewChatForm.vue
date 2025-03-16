e0a5cb493843cf5839c4f86f692add994c46e0a7
Lin-Htet-Phyo <linhtethtetphyo181688@gmail.com>
2025-03-16 18:52:57 +0630

    add NewChatForm

---
+++ src/components/NewChatForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <textarea
+    placeholder="Text message and press enter to send....."
+    v-model="message"
+    @keypress.enter="handleSubmit"
+  ></textarea>
+</template>
+
+<script>
+import { ref } from "vue";
+import getUser from "../composables/getUser";
+import { serverTimestamp } from 'firebase/firestore';
+
+export default {
+  setup() {
+    let message = ref("");
+    let { user } = getUser();
+    let handleSubmit = () => {
+      let chat = {
+        message: message.value,
+        name: user.value.displayName,
+        created_at: serverTimestamp()
+      };
+      console.log(chat);
+      message.value = "";
+    };
+
+    return {
+      message,
+      handleSubmit,
+    };
+  },
+};
+</script>
+
+<style>
+form {
+  margin: 10px;
+}
+
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
+</style>
