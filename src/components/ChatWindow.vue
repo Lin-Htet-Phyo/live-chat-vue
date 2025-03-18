aaaac20308a2d3556c0971d7415584ca6d827048
Lin-Htet-Phyo <linhtethtetphyo181688@gmail.com>
2025-03-18 21:55:26 +0630

    add chatwindow

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,19 +1,43 @@
 <template>
   <div class="chat-window">
-    <div class="messages">
-      <div class="single">
-        <span class="created-at">3mins ago</span>
-        <span class="name">mma</span>
-        <span class="message">mma</span>
+    <div class="messages" v-if="messages.length">
+      <div class="single" v-for="msg in messages" :key="msg.id">
+        <span class="created-at">{{
+          msg.created_at?.toDate().toLocaleString()
+        }}</span>
+        <span class="name">{{ msg.name }}</span>
+        <span class="message">{{ msg.message }}</span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import { db } from "../firebase/config";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+import { ref, onMounted } from "vue";
 
-}
+export default {
+  setup() {
+    let messages = ref([]); 
+
+    const messagesCollection = collection(db, "messages");
+    const messagesQuery = query(messagesCollection, orderBy("created_at"));
+
+    onMounted(() => {
+      onSnapshot(messagesQuery, (snapshot) => {
+        messages.value = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+      });
+    });
+
+    return {
+      messages,
+    };
+  },
+};
 </script>
 
 <style>
